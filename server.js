acdc8f662a4097cdabf3a6fc88dbc01d29dbb08a
Aryan <aryansd102@gmail.com>
2025-02-08 03:20:25 +0530

    Made changes in server port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ app.get('/test', (req, res) => {
     res.send("hello this is lease oye testing api");
 });
 
-const port = 3000;
-app.listen(3000, () => {
+const port = 8080;
+app.listen(8080, () => {
     console.log(`🚀 Server running on http://localhost:${port}`);
 })

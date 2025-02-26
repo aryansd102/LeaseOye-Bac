f753da3758743af7f0a0d2e1f652a1e868b972e4
Aryan <aryansd102@gmail.com>
2025-02-26 20:52:19 +0530

    Added signup config

---
+++ routes/auth.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+const router = express.Router();
+
+// User Signup
+router.post("/signup", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    if (!req.body) {
+        return res.status(400).json({message: "kya hero data toh daalo"});
+    }
+    const existingUser = await User.findOne({ email });
+    if (existingUser) return res.status(400).json({ message: "User already exists" });
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const newUser = new User({ name, email, password: hashedPassword });
+    await newUser.save();
+
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (err) {
+    res.status(500).json({ message: "Server Error", error: err.message });
+  }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,26 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 const express = require('express');
 const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+
+mongoose
+ .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
+
+const authRoutes = require("./routes/auth");
+app.use("/api/auth", authRoutes);
 
 app.get('/test', (req, res) => {
     res.send("hello this is lease oye testing api");
 });
 
-const port = 3000;
-app.listen(3000, () => {
+const port = 8080;
+app.listen(port, "0.0.0.0", () => {
     console.log(`🚀 Server running on http://localhost:${port}`);
 })
